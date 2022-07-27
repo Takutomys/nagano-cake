57fe793d5b0b1459c4cb150508fbe117d035764e
Takutomys <106647199+Takutomys@users.noreply.github.com>
2022-07-27 18:01:40 +0000

    admin-items-new

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
   resources :genres, only: [:index, :create, :edit, :update]
+  resources :items, only: [:new, :index, :show, :edit, :update]
   end
 end
