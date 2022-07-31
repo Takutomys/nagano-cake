69dc9b16e6243dae6f671045d3f8c213b72d505a
Takutomys <106647199+Takutomys@users.noreply.github.com>
2022-07-31 01:27:02 +0000

    Add admin-customers-show-edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,15 +4,17 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
-  
+
   private
   def item_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :adress, :telephon_number, :is_acti)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postal_code, :adress, :telephon_number, :is_active)
   end
-  
-  
+
+
 end
