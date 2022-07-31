4edf284703e63302e5fc2a27c113f1cf7c262c2c
Takutomys <106647199+Takutomys@users.noreply.github.com>
2022-07-31 08:00:23 +0000

    admin-top

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_26_214734) do
+ActiveRecord::Schema.define(version: 2022_07_31_055224) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -84,6 +84,29 @@ ActiveRecord::Schema.define(version: 2022_07_26_214734) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders_details", force: :cascade do |t|
+    t.integer "order_id", null: false
+    t.integer "item_id", null: false
+    t.integer "amount", null: false
+    t.integer "price", null: false
+    t.integer "making_status", default: 0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "adress", null: false
+    t.string "name", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "shopping_cost", null: false
+    t.integer "total_payment", null: false
+    t.integer "stasus", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
