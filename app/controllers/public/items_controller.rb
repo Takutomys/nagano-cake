6be73e183bebe26f060f912df336ba1c7a7ad833
Takutomys <106647199+Takutomys@users.noreply.github.com>
2022-08-13 19:03:51 +0000

    create-cart-item

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,12 +2,14 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page])
   end
-  
+
   def show
-  end   
-  
+    @cart_item = CartItem.new
+    @item = Item.find(params[:id])
+  end
+
    private
   def item_params
     params.require(:item).permit(:name, :introduction, :price, :is_active, :image, :genre_id)
-  end   
+  end
 end
